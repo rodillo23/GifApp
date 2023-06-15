cc84141d0cee05053c83677d3ac265ac18e915d4
Rodolfo Figueroa <ficr23@hotmail.com>
2023-06-15 17:24:26 -0500

    GifApp, sse muestran los gifs mediante una busqueda con fetch api, la consulta se hace a Giphy

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
+import { GifGrid } from "./components/GifGrid"
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['React'])
+
+  const onAddCategory = (newCategory) => {
+    if(categories.includes(newCategory)) return
+    setCategories([newCategory, ...categories])
+  }
+  
+  return (
+    <>
+      <h1>ExpertApp</h1>
+
+      <AddCategory onNewCategory={value => onAddCategory(value)}/>
+      
+      {
+        categories.map( category => (
+          <GifGrid key={category} category={category}/>
+        ))
+      }
+
+    </>
+  )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=ciYj5gEtmIln7s9bGcIw7QR9qsrOy0DQ&q=${category}&limit=10`
+    const resp = await fetch(url)
+    const {data} = await resp.json()
+    
+    const gifs = data.map(image => ({
+        id: image.id,
+        title: image.title,
+        url: image.images.downsized_medium.url
+    }))
+    
+    return gifs
+}
